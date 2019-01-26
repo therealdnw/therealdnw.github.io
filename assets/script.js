114422bdfab951bf94d2e6c197bba7b32c9f03b5
therealdnw <daniil.tashit@yandex.ru>
2019-01-26 19:50:56 +0500

    commentary

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+jQuery(function(){
+    jQuery('#home-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        dots: true
+    });
+});
+
