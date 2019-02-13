fc6f6c5b56d74dbbbf56401c9fbd3323ce09befc
therealdnw <47058626+therealdnw@users.noreply.github.com>
2019-02-13 19:21:45 +0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,1 @@
-jQuery(function(){
-    jQuery('#home-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        autoplay: true,
-        dots: true
-    });
-});
 
